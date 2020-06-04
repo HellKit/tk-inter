fadcb4ad91b1ea446370ebb8c1bdb5a8f64734fe
unknown <nofexx@bk.ru>
2020-06-04 17:52:24 +0300

    -ainit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Надо бы, что-то тут написать!')
+root.geometry("600x400+40+80")
+# conv = tk.Canvas(background='black', width=500, height=500)
+# conv.create_text('12090', 23)
+# conv.pack()
+root.mainloop()
