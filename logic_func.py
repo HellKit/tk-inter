9b4cbeae6982fcf7aa8faca30b228d522905d1e8
unknown <nofexx@bk.ru>
2020-06-06 15:03:10 +0300

    init

--- logic_func.py
+++ logic_func.py
@@ -1,103 +1,69 @@
 import os
 import re
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 
-BASE_DIR = os.getcwd()
-DATA_NOW = datetime.now().strftime('%d.%m.%Y').split('.')
+BASE_DIR = os.getcwd() # указываем текущую дерикторию
+DATA_TODAY = date.today() # указываем текущую дату
 
-YEAR = int(DATA_NOW[2])
-DAY = (
-    29 if YEAR % 4 == 0 or YEAR % 100 == 0 and YEAR % 400 == 0 else 28
-)
-YEAR_TYPE = (
-    366 if YEAR % 4 == 0 or YEAR % 100 == 0 and YEAR % 400 == 0 else 365
-)
-
-
-def get_month_days(day=DAY):
-    month_days = {
-        1: 31, 2: day, 3: 31,
-        4: 30, 5: 31, 6: 30,
-        7: 31, 8: 31, 9: 30,
-        10: 31, 11: 30, 12: 31
-    }
-    return month_days
-
-def read_file_and_get_date_and_text(file_name):
-    path = os.path.join(BASE_DIR, file_name)
-    rec = re.compile(r'(\d+\.\d+\.\d+)')
+def read_file_and_get_date_and_text(file_name: str) -> (list, list):
+    '''Возравщаем список дат и текста'''
+    path = os.path.join(BASE_DIR, file_name) # путь к файлу
+    rec = re.compile(r'(\d+\.\d+\.\d+)') # компилируем регулярное выражение
     with open(path, encoding='utf') as f:
-        data = f.readlines()
-        date = [rec.search(value)[0] for value in data]
-        text = [rec.split(value)[-1].strip() for value in data]
-        full_date_text = {val: text[idx] for idx, val in enumerate(date)}
-    return date, text, full_date_text
+        data = f.readlines() 
+        date_list = [rec.search(value)[0] for value in data] # список дат
+        text = [rec.split(value)[-1].strip() for value in data] # список тектса
+    return date_list, text
 
 
-
-def get_days_time(date):
-    days = []
-    for value in date:
-        value = value.split('.')
-        date_day, day_now = int(value[0]), int(DATA_NOW[0])
-        date_month, month_now = int(value[1]), int(DATA_NOW[1])
-        date_year, year_now = int(value[2]), int(DATA_NOW[2])
-        day = (
-            29 if date_year % 4 == 0 or date_year % 100 == 0 and date_year % 400 == 0 else 28
-        )
-        month_days = get_month_days(day)
-        if date_day >= day_now and date_month == month_now and date_year == year_now:
-            days.append(date_day - day_now)
-        elif (date_day >= day_now or date_month >= month_now) and date_year == year_now:
-            if date_month - month_now > 0:
-                for _ in range(date_month - month_now):
-                    days_month_now = (
-                        sum(
-                            [month_days[i] for i in range(month_now, date_month)]
-                        ) + date_day - day_now
-                    )
-                days.append(days_month_now)
-            elif date_month - month_now == 0:
-                days.append(month_days[month_now] - day_now + date_day)
-        elif date_day >= day_now or date_month >= month_now or date_year >= year_now:
-            if date_year - year_now > 0:
-                result = []
-                for _ in range(date_year - year_now):
-                    days_year_now = (
-                        YEAR_TYPE - sum(
-                            [month_days[i] for i in range(1, month_now)]
-                        ) - day_now
-                    )
-                    result.append(
-                        days_year_now + sum(
-                            [month_days[i] for i in range(1, date_month)]
-                        ) + date_day - 1
-                    )
-                days.append(sum(result))
-    return days
-
+def get_days_time(date_list: list) -> list:
+    '''Возвращаем количество дней в списке'''
+    return [
+            0 if ':' in str(year - DATA_TODAY).split()[0] 
+            else int(str(year - DATA_TODAY).split()[0]) 
+            for year in [date(int(val[0]), int(val[1]), int(val[2])) 
+            for val in [value.split('.') for value in date_list]]
+        ]
         
 
+def get_dict_corted_list(texts: list, days_list: list) -> dict:
+    '''Возвращаем отсортированные данные 
+    по возрастанию в виде словоря'''
+    dict_num_dict = {
+        days_list[idx]: text for idx, text in enumerate(texts)
+    } # записываем начальные данные в словарь key: value - (key - date, value - text)
+    days_list = sorted(days_list) # сортируем список
+    return {
+        days_list[idx]: dict_num_dict[days_list[idx]] 
+        for idx in range(len(dict_num_dict))
+    }
 
+def resultate_text_date(args: str, day: int) -> str:
+    '''Выводит дни в правильном склонении'''
+    if args == 'next':
+        args = ('Через')
+    if args == 'prev':
+        args = ('Прошел', 'Прошло')
+        
+    if day % 10 == 1 and day != 11:
+        return f'{args if len(args) != 2 else args[0]} {day} день :'
+    elif day % 10 in [2, 3, 4] and day // 10 not in [1]:
+        return f'{args if len(args) != 2 else args[1]} {day} дня :'
+    elif day == 0:
+        return 'Сегодня'
+    else:
+        return f'{args if len(args) != 2 else args[1]} {day} дней :'
 
 if __name__ == '__main__':
-    date, text, full_date_text = (
+    date_list, texts = (
         read_file_and_get_date_and_text('do.txt')
     )
-    # print(f'{date=}\n')
+    # print(date_list.)
+    # print(f'{date_list=}\n')
     # print(f'{text=}\n')
-    # print(f'{full_date_text=}\n')
-    month_days = get_month_days()
-    # print(month_days)
-    days = get_days_time(date)
-    if len(set(days)) == len(days):
-        result = {}
-        for idx, key in enumerate(days):
-            result[key] = text[idx]
+    days_list = get_days_time(date_list)
+    # print(days_list)
+    days_texts = get_dict_corted_list(texts, days_list)
+    print(days_texts)
     
-    last_result = {}
-    for day in set(days):
-        last_result[day] = result[day]
-
-    print(last_result)
--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 from tkinter import Tk, Canvas
 
+from logic_func import (
+    read_file_and_get_date_and_text,
+    get_days_time, get_dict_corted_list,
+    resultate_text_date
+)
 
-def root_settings(root):
+
+def root_settings():
+    root = Tk()
     root.title('Надо бы, что-то тут написать!')
-    root.geometry("700x800")
+    root.geometry("950x600")
     root.resizable(width=False, height=False)
+    return root
 
-def canva_settings(conv, root):
-    conv.create_text(350, 50, text="Мои текущие задачи",
+def canva_settings(root):
+    conv = Canvas(root, bg='black', width=4000, height=4000)
+    conv.create_text(450, 50, text="Мои текущие задачи",
                     justify='center', font="Verdana 28", fill='yellow')
-    conv.create_line(150, 70, 550, 70, fill='yellow')
+    conv.create_line(250, 70, 650, 70, fill='yellow')
+    return conv
 
-def message_print(conv):
-    for i in range(10):
-        i *= 30
-        conv.create_text(150, 100+i, text='Привет молодой человек',
-                        justify='center', font='Verdana 15', fill='yellow')
+
+def color_message(idx, data_text, text, color):
+    conv.create_text(450, 100+idx, text=f'{data_text} {text}',
+                    justify='center', font='Verdana 14', fill=f'{color}')
+
+def message_print(conv, days_texts: dict):
+    for idx, value in enumerate(days_texts.items()):
+        idx *= 30
+        if value[0] > 0:
+            text_date = resultate_text_date('next', value[0])
+        else:
+            text_date = resultate_text_date('prev', -value[0])
+        if value[0] > 0 and value[0] <= 7:
+            color_message(idx, text_date, value[1], 'yellow')
+        elif value[0] < 0:
+            color_message(idx, text_date, value[1], 'red')
+        elif value[0] > 7:
+            color_message(idx, text_date, value[1], 'white')
+
 
 
 if __name__ == '__main__':
-    root = Tk()
-    root_settings(root)
-    conv = Canvas(root, bg='black', width=4000, height=4000)
-    canva_settings(conv, root)
-    message_print(conv)
+
+    date_list, texts = (
+        read_file_and_get_date_and_text('do.txt')
+    )
+    days_list = get_days_time(date_list)
+    days_texts_dict = get_dict_corted_list(texts, days_list)
+
+    root = root_settings()
+    conv = canva_settings(root)
+    message_print(conv, days_texts_dict)
     conv.pack()
     root.mainloop()
