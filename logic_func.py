72c182184fe74bbdcac84b9016577fe1fdd04298
unknown <nofexx@bk.ru>
2020-06-10 16:03:05 +0300

    Update all functions

--- logic_func.py
+++ logic_func.py
@@ -3,68 +3,77 @@ import re
 from datetime import datetime, date, timedelta
 
 
-BASE_DIR = os.getcwd() # указываем текущую дерикторию
-DATA_TODAY = date.today() # указываем текущую дату
+BASE_DIR = os.getcwd()  # указываем текущую дерикторию
+COLORS = ('red', 'orange', 'yellow', 'white')
+
 
 def read_file_and_get_date_and_text(file_name: str) -> (list, list):
-    '''Возравщаем список дат и текста'''
-    path = os.path.join(BASE_DIR, file_name) # путь к файлу
-    rec = re.compile(r'(\d+\.\d+\.\d+)') # компилируем регулярное выражение
+    '''Возравщает список дат и текста'''
+    path = os.path.join(BASE_DIR, file_name)  # путь к файлу
+    rec = re.compile(r'(\d+\.\d+\.\d+)')  # компилируем регулярное выражение
     with open(path, encoding='utf') as f:
-        data = f.readlines() 
+        data = f.readlines()
         date_list = [rec.search(value)[0] for value in data]
         text = [rec.split(value)[-1].strip() for value in data]
     return date_list, text
 
 
 def get_days_time(date_l: list) -> list:
-    '''Возвращаем количество дней в списке'''
-    # Это было создано 06.06.2020
+    '''Возвращает количество дней в списке'''
     return [
-            0 if ':' in str(year - DATA_TODAY).split()[0] 
-            else int(str(year - DATA_TODAY).split()[0]) 
-            for year in [date(int(val[2]), int(val[1]), int(val[0])) 
-            for val in [value.split('.') for value in date_l]]
-        ]
-        
+        0 if ':' in str(year - date.today()).split()[0]
+        else int(str(year - date.today()).split()[0])
+        for year in [date(int(val[2]), int(val[1]), int(val[0]))
+                     for val in [value.split('.') for value in date_l]]
+    ]
+
 
 def get_dict_sorted_list(texts: list, days: list) -> dict:
-    '''Возвращаем отсортированные данные 
+    '''Возвращает отсортированные данные 
     по возрастанию в виде словоря'''
-    dict_num_list = {
-        days[idx]: text for idx, text in enumerate(texts)
-    } # записываем начальные данные в словарь key: value - (key - date, value - text)
-    days = sorted(days)
-    return {
-        days[idx]: dict_num_list[days[idx]] 
-        for idx in range(len(dict_num_list))
-    }
+    list_dict = [
+        {'days': days[idx], 'text': text}
+        for idx, text in enumerate(texts)
+    ]
+    return sorted(list_dict, key=lambda value: value['days'])
+
+
+def add_color(data: list) -> list:
+    '''Добавляет цвет для текста'''
+    for element in data:
+        if element['days'] < 0:
+            element['color'] = COLORS[0]
+        elif element['days'] == 0:
+            element['color'] = COLORS[1]
+        elif element['days'] < 7:
+            element['color'] = COLORS[2]
+        else:
+            element['color'] = COLORS[3]
+    return data
+
 
 def result_text_date(arg: str, day: int) -> str:
     '''Выводит дни в правильном склонении'''
     if arg == 'next':
         args = ('Остался', 'Осталось')
-    else: # 'prev'
+    else:  # 'prev'
         args = ('Прошел', 'Прошло')
-        
+
     if day % 10 == 1 and day != 11:
         return f'{args[0]} {day} день :'
-    elif day % 10 in [2, 3, 4] and day // 10 not in [1]:
+    elif day % 10 in [2, 3, 4] and day // 10 != 1:
         return f'{args[1]} {day} дня :'
     elif day == 0:
         return 'Сегодня :'
     else:
         return f'{args[1]} {day} дней :'
 
+
 if __name__ == '__main__':
     date_list, texts = (
         read_file_and_get_date_and_text('do.txt')
     )
-    # print(date_list.)
-    # print(f'{date_list=}\n')
-    # print(f'{text=}\n')
     days_list = get_days_time(date_list)
-    # print(days_list)
-    days_texts = get_dict_sorted_list(texts, days_list)
-    print(days_texts)
-    
+    list_dict = get_dict_sorted_list(texts, days_list)
+    list_dict = add_color(list_dict)
+    print(list_dict)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from tkinter import Tk, Canvas
-
 from logic_func import (
     read_file_and_get_date_and_text,
     get_days_time, get_dict_sorted_list,
-    result_text_date
+    result_text_date, add_color
 )
 
 
@@ -18,9 +17,9 @@ def root_settings():
 
 def canvas_settings(root):
     '''Создаем Canvas settings'''
-    canva = Canvas(root, bg='black', width=1920, height=1080)
+    canva = Canvas(root, bg='black', width=900, height=600)
     canva.create_text(450, 50, text='Мои текущие задачи',
-                    font='Verdana 28', fill='yellow')
+                      font='Verdana 28', fill='yellow')
     canva.create_line(250, 70, 650, 70, fill='yellow')
     return canva
 
@@ -28,37 +27,31 @@ def canvas_settings(root):
 def settings_message(idx: int, data_text: str, text: str, color: str):
     '''Для смены цветов и отображения текста'''
     canva.create_text(150, 100+idx, text=f'{data_text} {text}',
-                   anchor='w', font='Verdana 14', fill=f'{color}')
+                      anchor='w', font='Verdana 14', fill=f'{color}')
 
 
-def message_print(canva, days_texts: dict):
+def message_print(canva, days_texts: list):
     '''Главная функция по выводу в интерфейс приложения'''
-    for idx, value in enumerate(days_texts.items()):
+    for idx, element in enumerate(days_texts):
         idx *= 30
-        if value[0] > 0:
-            text_date = result_text_date('next', value[0])
+        if element['days'] > 0:
+            text_date = result_text_date('next', element['days'])
         else:
-            text_date = result_text_date('prev', -value[0])
-        if value[0] > 0 and value[0] <= 7:
-            settings_message(idx, text_date, value[1], 'yellow')
-        elif value[0] < 0:
-            settings_message(idx, text_date, value[1], 'red')
-        elif value[0] > 7:
-            settings_message(idx, text_date, value[1], 'white')
-        else:
-            settings_message(idx, text_date, value[1], 'orange')
+            text_date = result_text_date('prev', -element['days'])
+        settings_message(idx, text_date, element['text'], element['color'])
 
 
 if __name__ == '__main__':
     root = root_settings()
     canva = canvas_settings(root)
-    
+
     date_list, texts = (
         read_file_and_get_date_and_text('do.txt')
     )
     days_list = get_days_time(date_list)
-    days_texts_dict = get_dict_sorted_list(texts, days_list)
-    message_print(canva, days_texts_dict)
+    list_dict = get_dict_sorted_list(texts, days_list)
+    list_dict = add_color(list_dict)
+    message_print(canva, list_dict)
 
     canva.pack()
     root.mainloop()
